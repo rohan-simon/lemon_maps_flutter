98961eeee6bd22c837afb1902c99567d49953df0
brian y. <brian127@uw.edu>
2024-05-17 11:17:28 -0700

    Created compass_provider and position_provider files. Added basic compass_view framework.

---
+++ lib/views/compass_view.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:lemun/providers/compass_provider.dart';
+
+class CompassView extends StatefulWidget {
+  const CompassView({super.key});
+
+  @override
+  State<CompassView> createState() => _CompassViewState();
+}
+
+class _CompassViewState extends State<CompassView> {
+  @override
+  Widget build(BuildContext context) {
+    return Placeholder();
+  }
+  
+  @override
+  initState() {
+    super.initState();
+  }
+}
