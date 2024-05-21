8f08e6d7bb4741138796d7203eed02eb419755db
brian y. <brian127@uw.edu>
2024-05-21 12:44:28 -0700

    Updated vehicle.dart superclass to include three fields: vehicleType, longitude, and latitude.

--- lib/models/vehicle.dart
+++ lib/models/vehicle.dart
@@ -1,4 +1,10 @@
+import 'package:lemun/models/vehicle_types.dart';
+
 export 'lime.dart';
 export 'link_scooter.dart';
 
-abstract class Vehicle {}
+abstract class Vehicle {
+  VehicleType get vehicleType;
+  double get longitude;
+  double get latitude;
+}
