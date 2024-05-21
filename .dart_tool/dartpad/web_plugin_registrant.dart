3e417b8c675bc33e84fd24b49205fab1d14691f4
brian y. <brian127@uw.edu>
2024-05-21 12:15:11 -0700

    idk bro

--- .dart_tool/dartpad/web_plugin_registrant.dart
+++ .dart_tool/dartpad/web_plugin_registrant.dart
@@ -7,10 +7,12 @@
 // ignore_for_file: type=lint
 
 import 'package:geolocator_web/geolocator_web.dart';
+import 'package:permission_handler_html/permission_handler_html.dart';
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 void registerPlugins([final Registrar? pluginRegistrar]) {
   final Registrar registrar = pluginRegistrar ?? webPluginRegistrar;
   GeolocatorPlugin.registerWith(registrar);
+  WebPermissionHandler.registerWith(registrar);
   registrar.registerMessageHandler();
 }
