314260bcf13b00065d216bf6b918a6199a46a974
brian y. <brian127@uw.edu>
2024-05-22 16:12:44 -0700

    Added Daniel's map view

--- lib/views/map_view.dart
+++ lib/views/map_view.dart
@@ -3,45 +3,28 @@ import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:geolocator/geolocator.dart';
-import 'package:flutter_compass/flutter_compass.dart';
+import 'package:lemun/models/vehicle.dart';
+import 'package:lemun/models/vehicle_types.dart';
+import 'package:lemun/views/compass_view.dart';
 
-void main() => runApp(const ScaffoldExampleApp());
+class MapView extends StatefulWidget {
+  final List<Vehicle> vehicles;
 
-class ScaffoldExampleApp extends StatelessWidget {
-  const ScaffoldExampleApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: ScaffoldExample(),
-    );
-  }
-}
-
-class ScaffoldExample extends StatefulWidget {
-  const ScaffoldExample({super.key});
+  const MapView({super.key, required this.vehicles});
 
   @override
-  State<ScaffoldExample> createState() => _ScaffoldExampleState();
+  _MapViewState createState() => _MapViewState();
 }
 
-class _ScaffoldExampleState extends State<ScaffoldExample> {
+class _MapViewState extends State<MapView> {
   late final MapController _mapController;
-  LatLng _currentPosition = LatLng(51.509364, -0.128928); // Default to London;
-  double _currentHeading = 0.0;
-
-  List<LatLng> coordinates = [
-    LatLng(51.509364, -0.128928), // Example: London
-    LatLng(48.8566, 2.3522),      // Example: Paris
-    LatLng(40.7128, -74.0060),    // Example: New York
-  ];
+  LatLng _currentPosition = const LatLng(51.509364, -0.128928); // Default to London;
 
   @override
   void initState() {
     super.initState();
     _mapController = MapController();
     _getCurrentLocation();
-    _getDeviceOrientation();
   }
 
   Future<void> _getCurrentLocation() async {
@@ -65,56 +48,113 @@ class _ScaffoldExampleState extends State<ScaffoldExample> {
 
     setState(() {
       _currentPosition = LatLng(position.latitude, position.longitude);
-      _mapController.move(_currentPosition, 17.0);
+      _mapController.move(_currentPosition, 13.0);
     });
   }
 
-  void _getDeviceOrientation() {
-    FlutterCompass.events!.listen((CompassEvent event) {
-      setState(() {
-        _currentHeading = event.heading ?? 0.0;
-      });
-    });
-  }
-
-  List<Marker> createMarkers(List<LatLng> coordinates) {
-    return coordinates.map((coordinate) {
+  List<Marker> createVehicleMarkers(List<Vehicle> vehicles) {
+    print(vehicles.length);
+    return vehicles.map((vehicle) {
       return Marker(
         width: 80.0,
         height: 80.0,
-        point: coordinate,
-        rotate: true,
-        child: Icon(Icons.location_on, color: Colors.red, size: 40),
+        point: LatLng(vehicle.latitude, vehicle.longitude),
+        child: GestureDetector(
+          onTap: () {
+            Navigator.push(context, MaterialPageRoute(builder: (context) => CompassView(vehicle: vehicle)));
+          },
+          child: getVehicleIcon(vehicle.vehicleType)
+        )
       );
     }).toList();
   }
 
+  Icon getVehicleIcon(VehicleType type) {
+    switch (type) {
+    case VehicleType.bike:
+      return Icon(Icons.directions_bike, color: Colors.green, size: 40);
+    case VehicleType.scooter:
+      return Icon(Icons.electric_scooter, color: Colors.orange, size: 40);
+    case VehicleType.bus:
+      return Icon(Icons.directions_bus, color: Colors.blue, size: 40);
+    default:
+      return Icon(Icons.location_on, color: Colors.red, size: 40);
+    }
+  }
+
+  Widget buildLegend() {
+    return Container(
+      padding: EdgeInsets.all(10),
+      color: Colors.amber[100],
+      width: double.infinity, // Make the container fit the width of the screen
+      child: Column(
+        children: [
+          Text(
+            'Legend',
+            style: TextStyle(color: Colors.purple, fontSize: 20),
+          ),
+          SizedBox(height: 8,),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              legendItem(Icons.directions_bike, Colors.green, 'Bike'),
+              legendItem(Icons.electric_scooter, Colors.orange, 'Scooter'),
+              legendItem(Icons.directions_bus, Colors.blue, 'Bus'),
+              legendItem(Icons.catching_pokemon, Colors.red, 'You'),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget legendItem(IconData iconData, Color color, String label) {
+    return Column(
+      children: [
+        Icon(iconData, color: color, size: 30),
+        Text(
+          label,
+          style: const TextStyle(color: Colors.purple, fontSize: 10),
+        ),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return FlutterMap(
-      mapController: _mapController,
-      options: MapOptions(
-        initialCenter:  _currentPosition,
-        initialZoom: 17,
-      ),
+    return Column(
       children: [
-        TileLayer(
-          urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-          userAgentPackageName: 'com.example.app',
-        ),
-        MarkerLayer(
-          // marker for current location
-          markers: [
-            Marker(
-              width: 80,
-              height: 80,
-              point: _currentPosition,
-              rotate: false,
-                  // angle: _currentHeading * (3.14159265358979323846 / 180),
-              child: Icon(Icons.catching_pokemon, color: Colors.blue, size: 40),
+        Expanded(
+          flex: 5,
+          child: FlutterMap(
+            mapController: _mapController,
+            options: MapOptions(
+              initialCenter:  _currentPosition,
+              initialZoom: 9.2,
             ),
-            ...createMarkers(coordinates)
-          ]
+            children: [
+              TileLayer(
+                urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+                userAgentPackageName: 'com.example.app',
+              ),
+              MarkerLayer(
+                markers: [
+                  Marker(
+                    width: 80,
+                    height: 80,
+                    point: _currentPosition,
+                    rotate: false,
+                    child: Icon(Icons.catching_pokemon, color: Colors.red, size: 40),
+                  ),
+                  ...createVehicleMarkers(widget.vehicles)
+                ]
+              )
+            ],
+          ),
+        ),
+        Expanded(
+          flex: 1,
+          child: buildLegend()
         )
       ],
     );
