d9de92efa9126149fe0c47057d396232ac5ef53a
brian y. <brian127@uw.edu>
2024-05-26 17:05:35 -0700

    Removed commented code and unused imports from main

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:lemun/models/bus_stop.dart';
 import 'package:lemun/models/bus_stop_db.dart';
-import 'package:lemun/models/lime.dart';
-import 'package:lemun/models/vehicle_types.dart';
 import 'package:lemun/providers/drawing_provider.dart';
 import 'package:lemun/providers/opacity_provider.dart';
 import 'package:lemun/providers/position_provider.dart';
 import 'package:lemun/providers/scooter_provider.dart';
-import 'package:lemun/views/compass_view.dart';
 import 'package:lemun/views/home_page.dart';
-import 'package:lemun/views/map_view.dart';
 import 'package:provider/provider.dart';
 
 Future<BusStopDB> loadBusStopDB(String dataPath) async {
@@ -51,75 +46,3 @@ class LemunApp extends StatelessWidget {
     );
   }
 }
-
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-
-//   // This widget is the root of your application.
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Lemun',
-//       theme: ThemeData(
-//         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-//         useMaterial3: true,
-//       ),
-//       home: const MyHomePage(title: 'Flutter Demo Home Page'),
-//     );
-//   }
-// }
-
-// class MyHomePage extends StatefulWidget {
-//   const MyHomePage({super.key, required this.title});
-
-//   final String title;
-
-//   @override
-//   State<MyHomePage> createState() => _MyHomePageState();
-// }
-
-// class _MyHomePageState extends State<MyHomePage> {
-//   int _counter = 0;
-
-//   void _incrementCounter() {
-//     setState(() {
-//       ScooterChecker checker = ScooterChecker();
-//       checker.fetchLinkScooter();
-//       _counter++;
-//     });
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-
-//     return Scaffold(
-//       appBar: AppBar(
-
-//         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-
-//         title: Text(widget.title),
-//       ),
-//       body: Center(
-
-//         child: Column(
-
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: <Widget>[
-//             const Text(
-//               'You have pushed the button this many times:',
-//             ),
-//             Text(
-//               '$_counter',
-//               style: Theme.of(context).textTheme.headlineMedium,
-//             ),
-//           ],
-//         ),
-//       ),
-//       floatingActionButton: FloatingActionButton(
-//         onPressed: _incrementCounter,
-//         tooltip: 'Increment',
-//         child: const Icon(Icons.add),
-//       ), // This trailing comma makes auto-formatting nicer for build methods.
-//     );
-//   }
-// }
