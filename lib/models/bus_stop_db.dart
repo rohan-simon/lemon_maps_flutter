5a68e49edc7ddaa22d6297a2e178423065d6ebf0
brian y. <brian127@uw.edu>
2024-05-28 12:47:22 -0700

    Added overflow for legend text

--- lib/models/bus_stop_db.dart
+++ lib/models/bus_stop_db.dart
@@ -19,30 +19,15 @@ class BusStopDB {
 
   // Private helper to parse csv string into a list of bus stops
   static List<BusStop> _decodeBusStopList(String csvString) {
-
     List<BusStop> busStops = [];
     var listOfRows = csvString.split('\n');
 
-
     for (var row in listOfRows) {
-
-      
-
       List elemList = row.split(',');
-
       if (elemList.length > 1) {
-
         busStops.add(BusStop(name: elemList[2], latitude: double.parse(elemList[4]), longitude: double.parse(elemList[5])));
-
-      }
-
-
-      
+      }      
     }
-    
     return busStops;
-
   }
-  
-
 }
